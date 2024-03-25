0bbe569adc1a4133fa8db72cf50d929484412d32
David <dajulian@gmail.com>
2024-03-25 00:12:34 -0400

    - user parameters are in a different file - Yaw Bicopter includes manual input

--- swarmbase/RawBicopterJoystick.py
+++ swarmbase/RawBicopterJoystick.py
@@ -5,18 +5,8 @@ from joystick.JoystickManager import JoystickManager
 from gui.simpleGUI import SimpleGUI
 import time
 
-##### Insert your robot's MAC ADDRESS here ####
-## (you can get it by running your arduino and looking at the serial monitor for your flying drone) ##
-ROBOT_MAC = "34:85:18:8D:A0:D4" # "DC:54:75:D7:B3:E8"
-### Insert your SERIAL PORT here ###
-## may look like "COM5" in windows or "/dev/tty.usbmodem14301" in mac  #
-## look in arduino for the port that your specific transeiver is connected to  ##
-## Note: make sure that your serial monitor is OFF in arduino or else you will get "access is denied" error. ##
-PORT = "/dev/ttyUSB0"
+from user_parameters import ROBOT_MAC, SERIAL_PORT, PRINT_JOYSTICK
 
-
-# For debug purposes
-PRINT_JOYSTICK = False
 
 
 BaseStationAddress = "" # you do not need this, just make sure your DroneMacAddress is not your base station mac address
@@ -25,7 +15,7 @@ BaseStationAddress = "" # you do not need this, just make sure your DroneMacAddr
 
 if __name__ == "__main__":
     # Communication
-    serial = SerialController(PORT, timeout=.1)  # 5-second timeout
+    serial = SerialController(SERIAL_PORT, timeout=.1)  # 5-second timeout
     serial.manage_peer("A", ROBOT_MAC)
     serial.manage_peer("G", ROBOT_MAC)
     serial.send_preference(ROBOT_MAC, DataType_Float, "kpz", .6)
