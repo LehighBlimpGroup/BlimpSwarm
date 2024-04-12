1b82ccc9bb54ae4cc0e592a581077c186417c099
nuryte <positronic83@gmail.com>
2024-04-11 20:46:15 -0400

    Modifications for camera and LED

--- Nicla/BaseIbusNicla.py
+++ Nicla/BaseIbusNicla.py
@@ -1,7 +1,23 @@
 
 
-from pyb import UART
+from pyb import UART, LED
 
+import sensor
+import time
+
+sensor.reset()  # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.RGB565)  # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.ioctl(sensor.IOCTL_SET_FOV_WIDE, True)
+sensor.set_framesize(sensor.HQVGA)  # Set frame size to QVGA (320x240)
+sensor.skip_frames(time=2000)  # Wait for settings take effect.
+
+r_LED = LED(1)  # The red LED
+g_LED = LED(2)  # The green LED
+b_LED = LED(3)  # The blue LED
+g_LED.on()
+r_LED.off()
+b_LED.off()
+clock = time.clock()  # Create a clock object to track the FPS.
 uart = UART("LP1", 115200, timeout_char=2000) # (TX, RX) = (P1, P0) = (PB14, PB15)
 
 def checksum(arr, initial= 0):
@@ -28,7 +44,7 @@ def IBus_message(message_arr_to_send):
     chA, chB = checksum(msg[:-2], 0)
     msg[-1] = chA
     msg[-2] = chB
-    
+
     uart.write(msg)
 
 def refreshIbusConnection():
@@ -36,28 +52,42 @@ def refreshIbusConnection():
         uart_input = uart.read()
 
 
-############### Color detection here ###############
-
-#put color detection and such here
 
 
+while True:
+    ############### Color detection here ###############
 
-color_is_detected = False # replace this with your method
+    #put color detection and such here
 
-
-flag = 0
-if (color_is_detected):
-    flag = 1 # when a color is detected make this flag 1
-pixels_x = 0 # put your x center in pixels
-pixels_y = 0 # put your y center in pixels
-pixels_w = 0 # put your width in pixels (these have almost no affect on control (for now))
-pixels_h = 0 # put your height center in pixels (these have almost no affect on control (for now))
-###############################
+    clock.tick()  # Update the FPS clock.
+    img = sensor.snapshot()  # Take a picture and return the image.
+    print(clock.fps())  # Note: OpenMV Cam runs about half as fast when connected
+    # to the IDE. The FPS should increase once disconnected.
 
 
+    color_is_detected = False # replace this with your method
 
 
-messageToSend = [flag, pixels_x, pixels_y, pixels_w, pixels_h]
+    flag = 0
+    if (color_is_detected):
+        g_LED.off()
+        r_LED.off()
+        b_LED.on()
+        flag = 1 # when a color is detected make this flag 1
+    else:
+        g_LED.on()
+        r_LED.off()
+        b_LED.off()
+    pixels_x = 0 # put your x center in pixels
+    pixels_y = 0 # put your y center in pixels
+    pixels_w = 0 # put your width in pixels (these have almost no affect on control (for now))
+    pixels_h = 0 # put your height center in pixels (these have almost no affect on control (for now))
+    ###############################
 
-IBus_message(messageToSend)
-refreshIbusConnection()
+
+
+
+    messageToSend = [flag, pixels_x, pixels_y, pixels_w, pixels_h]
+
+    IBus_message(messageToSend)
+    refreshIbusConnection()
