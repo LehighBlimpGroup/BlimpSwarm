aa1732a34c167bb39d8fbaf2bdafe7b31b726877
Subhadeep Koley <ksubhadeepwork@gmail.com>
2025-11-12 16:46:56 -0500

    Added mocap data collection. TODO: Compare with IMU data

--- swarmbase/RobotControl.py
+++ swarmbase/RobotControl.py
@@ -6,9 +6,23 @@ import Preferences
 import time
 import importlib
 import pickle as pkl
+from optitrack_natnet.NatNetClient import NatNetClient
+from optitrack_natnet.util import quaternion_to_euler
 
 PRINT_JOYSTICK = False
 
+positions = {}
+rotations = {}
+
+
+# This is a callback function that gets connected to the NatNet client. It is called once per rigid body per frame
+def receive_rigid_body_frame(id, position, rotation_quaternion):
+    # Position and rotation received
+    positions[id] = position
+    # The rotation is in quaternion. We need to convert it to euler angles
+    rotx, roty, rotz = quaternion_to_euler(rotation_quaternion)
+    # Store the roll pitch and yaw angles
+    rotations[id] = (rotx, roty, rotz)
 
 def startAutonomousBall(serial, robot, args):
     serial.send_control_params(robot, (3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
@@ -124,6 +138,13 @@ def sendAttackerPreferences(serial, robot, args):
     serial.send_control_params(robot, (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0))
     return -1
 
+def receive_rigid_body_frame(id, position, rotation_quaternion):
+    # Position and rotation received
+    positions[id] = position
+    # The rotation is in quaternion. We need to convert it to euler angles
+    rotx, roty, rotz = quaternion_to_euler(rotation_quaternion)
+    # Store the roll pitch and yaw angles
+    rotations[id] = (rotx, roty, rotz)
 
 def main():
     try:
@@ -161,11 +182,31 @@ def main():
         angle = 45
         sensor_data_all = []
 
+        # Mocap data
+
+        clientAddress = "192.168.0.5"
+        optitrackServerAddress = "192.168.0.4"
+        robot_id = 525
+
+        # This will create a new NatNet client
+        streaming_client = NatNetClient()
+        streaming_client.set_client_address(clientAddress)
+        streaming_client.set_server_address(optitrackServerAddress)
+        streaming_client.set_use_multicast(True)
+        # Configure the streaming client to call our rigid body handler on the emulator to send data out.
+        streaming_client.rigid_body_listener = receive_rigid_body_frame
+
+        # Start up the streaming client now that the callbacks are set up.
+        # This will run perpetually, and operate on a separate thread.
+        is_running = streaming_client.run()
+
         while True:
+            # time.sleep(0.02)
             keys = robot_master.get_last_n_keys(1)
             axis, buttons = joystick.getJoystickInputs()
             sensor_data = robot_master.processManual(axis, buttons, print_vals=True)
-            sensor_data_all.append(sensor_data)
+            mocap = positions[robot_id] + rotations[robot_id]
+            sensor_data_all.append((sensor_data, mocap))
 
             power = min(1, max(0, power))
             angle = min(180, max(-180, angle))
